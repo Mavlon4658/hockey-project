b3da92a659d2a8726ba2a5a4c97d34ae035013ac
Mavlon4658 <mavlonbe4658>
2025-02-01 20:56:28 +0500

    ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
-// partner
+const bodyHidden = () => {
+  document.querySelector('body').style.overflow = 'hidden';
+}
+
+const bodyVisible = () => {
+  document.querySelector('body').style.overflow = 'visible';
+}
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuBg = document.querySelector('.mobile-menu__bg');
+const bars = document.querySelector('.header .bars');
+
+bars.onclick = () => {
+  bodyHidden();
+  mobileMenu.classList.toggle('active');
+  bars.classList.toggle('active');
+}
+
+mobileMenuBg.onclick = () => {
+  bodyHidden();
+  mobileMenu.classList.remove('active');
+  bars.classList.remove('active');
+}
+
 var partnerslider = new Swiper(".partnerSwiper", {
   slidesPerView: 1.8,
   spaceBetween: 15,
   centeredSlides: true,
+  initialSlide: 1,
   breakpoints: {
     768: {
       slidesPerView: 2.8,
@@ -13,10 +37,7 @@ var partnerslider = new Swiper(".partnerSwiper", {
     },
   },
 });
-partnerslider.slideTo(1);
-// partner
 
-// program
 var partnerslider = new Swiper(".programSwiper", {
   slidesPerView: 1.2,
   spaceBetween: 15,
@@ -35,7 +56,7 @@ var partnerslider = new Swiper(".programSwiper", {
     },
   },
 });
-// program
+
 const participateSwp = new Swiper('.participate .swiper', {
     slidesPerView: 'auto',
     spaceBetween: 20,
