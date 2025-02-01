821276ff20264a82fd8b1e4f74feddf8feea7f9b
Mavlon4658 <mavlonbe4658>
2025-02-01 15:54:13 +0500

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const participateSwp = new Swiper('.participate .swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    breakpoints: {
+        993: {
+            slidesPerView: 4,
+        }
+    }
+})
